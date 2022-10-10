f95ca477d7ede4b53a9b9a4e644ba8c88fcd3e40
manmeenkaur <manmeen.kaur.17@gmail.com>
2022-10-10 01:06:04 -0400

    Implemented root view controller and the content views.

--- View Switcher/BlueViewController.swift
+++ View Switcher/BlueViewController.swift
@@ -14,7 +14,15 @@ class BlueViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+    @IBAction func blueButtonPressed(sender: UIButton) {
+            let alert = UIAlertController(title: "Blue View Button Pressed",
+                                          message: "You pressed the button on the blue view",
+                                          preferredStyle: .alert)
+            let action = UIAlertAction(title: "Yes, I did", style: .default,
+                                       handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
 
     /*
     // MARK: - Navigation
--- View Switcher/YellowViewController.swift
+++ View Switcher/YellowViewController.swift
@@ -15,6 +15,15 @@ class YellowViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func yellowButtonPressed(sender: UIButton) {
+            let alert = UIAlertController(title: "Yellow View Button Pressed",
+                                          message: "You pressed the button on the yellow view",
+                                          preferredStyle: .alert)
+            let action = UIAlertAction(title: "Yes, I did", style: .default,
+                                       handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
 
     /*
     // MARK: - Navigation
